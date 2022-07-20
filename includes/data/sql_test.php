0a9d66d3cc69f0a518a5819c8d54114e24de7612
Tom Barkley <thomascbarkley@hotmail.com>
2022-07-20 11:57:07 -0400

    SQL test update

--- includes/data/sql_test.php
+++ includes/data/sql_test.php
@@ -7,16 +7,14 @@
     );
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
-         FROM [SalesLT].[ProductCategory] pc
-         JOIN [SalesLT].[Product] p
-         ON pc.productcategoryid = p.productcategoryid";
+    $tsql= "SELECT TOP 20 c.pid as PID, c.description as Description
+         FROM [dbo].[catalog] c";
     $getResults= sqlsrv_query($conn, $tsql);
     echo ("Reading data from table" . PHP_EOL);
     if ($getResults == FALSE)
         echo (sqlsrv_errors());
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-     echo ($row['CategoryName'] . " " . $row['ProductName'] . PHP_EOL);
+     echo ($row['PID'] . " " . $row['Description'] . PHP_EOL);
     }
     sqlsrv_free_stmt($getResults);
 ?>
