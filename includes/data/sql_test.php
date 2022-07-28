b27a247f972d597e740f9a1cd416c70a400f348e
Tom Barkley <thomascbarkley@hotmail.com>
2022-07-28 12:04:25 -0400

    sql_test update #4 7/28/2022

--- includes/data/sql_test.php
+++ includes/data/sql_test.php
@@ -9,13 +9,13 @@
     );
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
-    $tsql= "SELECT TOP 20 SELECT pid, image, image_schematics, description, weight, price FROM [dbo].[catalog]";
+    $tsql= "SELECT TOP 20 SELECT pid, description FROM [dbo].[catalog]";
     $getResults= sqlsrv_query($conn, $tsql);
     echo ("Reading data from table" .PHP_EOL);
 
     $html="<table>";
     
-    echo ("After HTML string definition");
+    echo ("After HTML -- ");
 
     echo ($getResults);
 
@@ -23,20 +23,16 @@
         echo (sqlsrv_errors());
     while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
         echo ("in Row");
-        echo ($row['pid'] . " " . $row['description'] .PHP_EOL);
-    
-
-    
+        echo ($row);
+        //echo ($row['pid'] . " " . $row['description'] .PHP_EOL);
     /*
-    $html .= "<tr>
-     <td >" . $row['pid'] . "<br>" . $row['image'] . "<br>" . $row['image_schematics'] ."</td>
-     <td >" . $row['description'] . "</td>
-     <td >" . $row['weight'] . "</td>
-     <td >" . $row['price'] . "</td>
-   </tr>";
+    $html .= "<tr>";
+    $html .= "<td >" . $row['pid'] . "<br>" . $row['image'] . "<br>" . $row['image_schematics'] ."</td>";
+    $html .= "<td >" . $row['description'] . "</td>";
+    $html .= "<td >" . $row['weight'] . "</td>";
+    $html .= "<td >" . $row['price'] . "</td>";
+    $html .= "</tr>";
    */
-   
-
     }
     sqlsrv_free_stmt($getResults);
    $html .= "</table>test";
