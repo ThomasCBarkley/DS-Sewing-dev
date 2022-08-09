c1a6db7e2a745a4dad5c32d33d927ed5879d3b0e
Tom Barkley <thomascbarkley@hotmail.com>
2022-08-09 10:19:37 -0400

    Cookie update #6

--- includes/data/getprice.php
+++ includes/data/getprice.php
@@ -5,6 +5,11 @@ $value = 'Test Cookie Session ID';
 setcookie("TestCookie", $value);
 //setcookie("TestCookie", $value, time()+3600);  /* expire in 1 hour */
 
+$cookie_name = "user";
+$cookie_value = "John Doe";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+
+
 final class DB {
 
     public static function init()
@@ -193,6 +198,14 @@ function getDetailLine($pid)
     $rtn="";
     $SESS_ID = $_COOKIE["TestCookie"];
     echo $_COOKIE["TestCookie"];
+    /****************************************************************************/
+    if(!isset($_COOKIE[$cookie_name])) {
+        echo "Cookie named '" . $cookie_name . "' is not set!";
+    } else {
+        echo "Cookie '" . $cookie_name . "' is set!<br>";
+        echo "Value is: " . $_COOKIE[$cookie_name];
+    }
+    /*****************************************************************************/
     
     if ($res == FALSE)
         $rtn = sqlsrv_errors();
