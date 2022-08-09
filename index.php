7a858c03c6a3a97c91c45f6c29cd5546d9ad439a
Tom Barkley <thomascbarkley@hotmail.com>
2022-08-09 13:16:18 -0400

    Seed seesion_id() #3

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $description = "Truck covers,tarps and accessories for trucking fleets and indep
 $robots = "index,follow";
 $is_home_page = true;
 
-session_start();
+//session_start();
 
 ?>
   <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/includes/head.php"; ?>
