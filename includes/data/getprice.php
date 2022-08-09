0ba622bac607a02a7e462dadc651e8d0ea2282ee
Tom Barkley <thomascbarkley@hotmail.com>
2022-08-09 10:39:20 -0400

    Session ID Update #2

--- includes/data/getprice.php
+++ includes/data/getprice.php
@@ -5,15 +5,16 @@ $id = session_id();
 
 echo ($id);
 
+/*
 $value = 'Test Cookie Session ID';
 
 setcookie("TestCookie", $value);
-//setcookie("TestCookie", $value, time()+3600);  /* expire in 1 hour */
-
+setcookie("TestCookie", $value, time()+3600);  /* expire in 1 hour */
+/*
 $cookie_name = "user";
 $cookie_value = "John Doe";
 setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
-
+*/
 
 final class DB {
 
@@ -201,15 +202,12 @@ function getDetailLine($pid)
     
     //$html='';
     $rtn="";
-    $SESS_ID = $_COOKIE["TestCookie"];
-    echo $_COOKIE["TestCookie"];
+    $SESS_ID = '';
+    echo ('Session ID = ' .$id);
+
+    
     /****************************************************************************/
-    if(!isset($_COOKIE[$cookie_name])) {
-        echo "Cookie named '" . $cookie_name . "' is not set!";
-    } else {
-        echo "Cookie '" . $cookie_name . "' is set!<br>";
-        echo "Value is: " . $_COOKIE[$cookie_name];
-    }
+
     /*****************************************************************************/
     
     if ($res == FALSE)
