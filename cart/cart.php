ba7b29e20f4602bc949579cad5a8319008d5f551
Tom Barkley <thomascbarkley@hotmail.com>
2022-08-10 14:35:55 -0400

    BUILD AND DEPLOY CART FUNCTION

--- cart/cart.php
+++ cart/cart.php
@@ -12,7 +12,7 @@ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 */
 
 //Added by TCB
-function addToCart($pid, $price, $weight, $qty, $length, $width, $height){
+/* function addToCart($pid, $price, $weight, $qty, $length, $width, $height){
     $serverName = "ds-sewing-dev-server.database.windows.net"; // update me
     
     $connectionOptions = array(
@@ -27,7 +27,7 @@ function addToCart($pid, $price, $weight, $qty, $length, $width, $height){
     VALUES ($_SESSION, $pid, $weight, $qty, $length, $width, $height)";
     return "success";
     
-}
+} */
 
 
 //get action string
@@ -37,7 +37,7 @@ $action = isset($_GET['action'])?$_GET['action']:"";
 if($action=='addcart' && $_SERVER['REQUEST_METHOD']=='POST') {
 	
 	//Finding the product by code
-	$query = "SELECT * FROM products WHERE sku=:sku";
+/* 	$query = "SELECT * FROM products WHERE sku=:sku";
 	$stmt = $conn->prepare($query);
 	$stmt->bindParam('sku', $_POST['sku']);
 	$stmt->execute();
@@ -46,33 +46,33 @@ if($action=='addcart' && $_SERVER['REQUEST_METHOD']=='POST') {
 	$currentQty = $_SESSION['products'][$_POST['sku']]['qty']+1; //Incrementing the product qty in cart
 	$_SESSION['products'][$_POST['sku']] =array('qty'=>$currentQty,'name'=>$product['name'],'image'=>$product['image'],'price'=>$product['price']);
 	$product='';
-	header("Location:shopping-cart.php");
+	header("Location:shopping-cart.php"); */
 }
 
 //Empty All
 if($action=='emptyall') {
-	$_SESSION['products'] =array();
+/* 	$_SESSION['products'] =array();
 	header("Location:shopping-cart.php");	
-}
+ */}
 
 //Empty one by one
 if($action=='empty') {
-	$sku = $_GET['sku'];
+/* 	$sku = $_GET['sku'];
 	$products = $_SESSION['products'];
 	unset($products[$sku]);
 	$_SESSION['products']= $products;
 	header("Location:shopping-cart.php");	
-}
+ */}
 
 
  
  
  //Get all Products
-$query = "SELECT * FROM products";
+/* $query = "SELECT * FROM products";
 $stmt = $conn->prepare($query);
 $stmt->execute();
 $products = $stmt->fetchAll();
-
+ */
 ?>
 
 <!DOCTYPE html>
@@ -88,61 +88,7 @@ $products = $stmt->fetchAll();
 </head>
 <body>
 <div class="container" style="width:600px;">
-  <?php if(!empty($_SESSION['products'])):?>
-  <nav class="navbar navbar-inverse" style="background:#04B745;">
-    <div class="container-fluid pull-left" style="width:300px;">
-      <div class="navbar-header"> <a class="navbar-brand" href="#" style="color:#FFFFFF;">Shopping Cart</a> </div>
-    </div>
-    <div class="pull-right" style="margin-top:7px;margin-right:7px;"><a href="shopping-cart.php?action=emptyall" class="btn btn-info">Empty cart</a></div>
-  </nav>
-  <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>Image</th>
-        <th>Name</th>
-        <th>Price</th>
-        <th>Qty</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <?php foreach($_SESSION['products'] as $key=>$product):?>
-    <tr>
-      <td><img src="<?php print $product['image']?>" width="50"></td>
-      <td><?php print $product['name']?></td>
-      <td>$<?php print $product['price']?></td>
-      <td><?php print $product['qty']?></td>
-      <td><a href="shopping-cart.php?action=empty&sku=<?php print $key?>" class="btn btn-info">Delete</a></td>
-    </tr>
-    <?php $total = $total+$product['price'];?>
-    <?php endforeach;?>
-    <tr><td colspan="5" align="right"><h4>Total:$<?php print $total?></h4></td></tr>
-  </table>
-  <?php endif;?>
-  <nav class="navbar navbar-inverse" style="background:#04B745;">
-    <div class="container-fluid">
-      <div class="navbar-header"> <a class="navbar-brand" href="#" style="color:#FFFFFF;">Products</a> </div>
-    </div>
-  </nav>
-  <div class="row">
-    <div class="container" style="width:600px;">
-      <?php foreach($products as $product):?>
-      <div class="col-md-4">
-        <div class="thumbnail"> <img src="<?php print $product['image']?>" alt="Lights">
-          <div class="caption">
-            <p style="text-align:center;"><?php print $product['name']?></p>
-            <p style="text-align:center;color:#04B745;"><b>$<?php print $product['price']?></b></p>
-            <form method="post" action="shopping-cart.php?action=addcart">
-              <p style="text-align:center;color:#04B745;">
-                <button type="submit" class="btn btn-warning">Add To Cart</button>
-                <input type="hidden" name="sku" value="<?php print $product['sku']?>">
-              </p>
-            </form>
-          </div>
-        </div>
-      </div>
-      <?php endforeach;?>
-    </div>
-  </div>
-</div>
+IT LOADS
+</DIV>
 </body>
 </html>
