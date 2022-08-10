a98a3e149686d1f4f0052ae624756cb0268315b9
Tom Barkley <thomascbarkley@hotmail.com>
2022-08-10 15:11:46 -0400

    added jQuery

--- truck_acc/cabguard/cabguard1.php
+++ truck_acc/cabguard/cabguard1.php
@@ -23,6 +23,7 @@
 		addToCart($pid, $price, $weight, $qty, $length, $width, $height) function in the /includes/data/getprice.php file
 		
 	-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 </head>
 <body>
