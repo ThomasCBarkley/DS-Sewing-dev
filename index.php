19035c577cb8d9d96e4dd025d5d33e71d745b127
Tom Barkley <thomascbarkley@hotmail.com>
2022-08-10 12:16:14 -0400

    and now i'm really pissed off....

--- index.php
+++ index.php
@@ -19,6 +19,8 @@ echo ('New ID = ' . $_SESSION['RandomNumber']);
 $TEST = session_create_id('test');
 echo $TEST;
 echo ('session_id ' . session_id());
+echo('PHPSESSID=' . $_COOKIE["PHPSESSID"]);
+
 ?>
   <?php require_once $_SERVER["DOCUMENT_ROOT"] . "/includes/head.php"; ?>
 </head>
