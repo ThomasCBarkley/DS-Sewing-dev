48cdf3c82f9f095132b4b539b9dcdb61f5d9a210
Tom Barkley <thomascbarkley@hotmail.com>
2022-08-18 12:23:20 -0400

    update cart eventhandler functions #8

--- cart/EventHandler.js
+++ cart/EventHandler.js
@@ -27,7 +27,7 @@ function updateButton(pid, rowcount){
     alert(rowcount +" value=" + qty);
     */
     
-    for (let i = 0; i <= rowcount; i++) {
+    for (let i = 1; i <= rowcount; i++) {
         alert(i);
         //if (i === 3) { break; }
         //text += "The number is " + i + "<br>";
